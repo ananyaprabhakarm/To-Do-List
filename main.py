0c572c57ad837524d42d8802c8ef19525abb016c
ananyaprabhakarm <ananyaprabhakarm@gmail.com>
2024-01-07 20:29:50 +0530

    improved function

--- main.py
+++ main.py
@@ -2,12 +2,44 @@
 from tkinter import *
 import tkinter.messagebox
 
-# CREATING INITIAL WINDOW
-window = Tk()
-window.title("To-Do")
+
+# TO DO FUNCTIONS
+def entertask():
+  user_input = ""
+  def add():
+    user_input = entry.get(1.0, "end-1c")
+    if user_input == "":
+      tkinter.messagebox.showwarning(title="Warning!" ,message="Enter the text")
+    else:
+      listbox.insert(END, user_input)
+      entry.delete(0, END)
+
+  root1 = Toplevel()
+  root1.title("Add Task")
+  entry = Text(root1, width=40)
+  entry.pack()
+  button = Button(root1,width=40, height=2,text="Add", command=add)
+  button.pack()
+  root1.mainloop()
+      
+def deletetask():
+  select = listbox.curselection()
+  listbox.delete(select)
+
+def markscomplete():
+  mark = listbox.curselection()
+  temp = mark[0]
+  marked = listbox.get(mark)
+  marked = marked + " ✔"
+  listbox.delete(temp)
+  listbox.insert(temp,marked)
+
+# CREATING WINDOW
+root = Tk()
+root.title("To-Do")
 
 # FRAME WIDGET
-frame = Frame(window)
+frame = Frame(root)
 frame.pack()
 
 # HOLDING ITEMS IN A LISTBOX
@@ -20,50 +52,17 @@ scroll.pack(side=tkinter.RIGHT,fill=tkinter.Y)
 listbox.config(yscrollcommand=scroll.set)
 scroll.config(command=listbox.yview)
 
+
 # BUTTONWIDGET
-entryButton = Button(window, text="Add Task", width=50)
+entryButton = Button(root, text="Add Task", width=50, command=entertask)
 entryButton.pack(pady=3)
-
-deleteButton = Button(window, text="Delete Selected Task", width=50)
+deleteButton = Button(root, text="Delete Selected Task", width=50, command=deletetask)
 deleteButton.pack(pady=3)
-
-markButton = Button(window, text="Mark as completed", width=50)
+markButton = Button(root, text="Mark as completed", width=50, command=markscomplete)
 markButton.pack(pady=3)
 
-window.mainloop()
-
-# TO DO FUNCTIONS
-def entertask():
-  input = ""
-  def add():
-    input = entry.get(1.0, "end-1c")
-    if input == "":
-      tkinter.messagebox.showwarning(title="Warning!" ,message="Enter the text")
-    else:
-      listbox.insert(END,input)
-      root1.destroy()
-
-root1 = Tk()
-root1.title("Add Task")
-entry = Text(root1, width=40, height=4)
-entry.pack()
-button = Button(root1, text="Add Task")
-button.pack()
-root1.mainloop()
+root.mainloop()
 
 
-def deletetask():
-  select = listbox.curselection()
-  listbox.delete(select[0])
 
 
-def markscomplete():
-  mark = listbox.curselection()
-  temp = mark[0]
-
-  marked = listbox.get(mark)
-
-  marked = marked + " ✔"
-
-  listbox.delete(temp)
-  listbox.insert(temp,marked)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+entry = Text(root, width=45, height=2)
+# entry.pack()
